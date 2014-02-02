c16945ad85b6fdabe0806a4fc2c755448587a472
DQNEO <dqneoo@gmail.com>
2014-02-03 02:33:37 +0900

    initial commit

---
+++ site-cookbooks/yum-remi/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: yum-remi
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "yum"
+
+# add epel repository
+yum_repository 'epel' do
+  description 'Extra Packages for Enterprise Linux'
+  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  fastestmirror_enabled true
+  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+  action :create
+end
+
+# add remi repository
+yum_repository 'remi' do
+  description 'Les RPM de Remi - Repository'
+  baseurl 'http://rpms.famillecollet.com/enterprise/6/remi/x86_64/'
+  gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
+  fastestmirror_enabled true
+  action :create
+end
+
+# add remi-php55 repository
+yum_repository 'remi-php55' do
+  description 'Les RPM de Remi PHP55 - Repository'
+  mirrorlist 'http://rpms.famillecollet.com/enterprise/6/php55/mirror'
+  gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
+  fastestmirror_enabled true
+  action :create
+end
+
+%w{php php-pear php-mbstring php-xml php-devel php-mysql php-gd}.each do |pkg|
+    package pkg do
+        options "--enablerepo=remi-php55"
+        action :install
+    end
+end
