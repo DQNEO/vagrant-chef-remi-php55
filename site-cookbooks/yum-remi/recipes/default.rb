520a0e0fedacca0d3449931809b816184fcc3e55
DQNEO <dqneoo@gmail.com>
2014-02-04 11:49:01 +0900

    use mirrorlist instead of baseurl

--- site-cookbooks/yum-remi/recipes/default.rb
+++ site-cookbooks/yum-remi/recipes/default.rb
@@ -21,7 +21,7 @@ end
 # add remi repository
 yum_repository 'remi' do
   description 'Les RPM de Remi - Repository'
-  baseurl 'http://rpms.famillecollet.com/enterprise/6/remi/x86_64/'
+  mirrorlist 'http://rpms.famillecollet.com/enterprise/6/remi/mirror'
   gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
   fastestmirror_enabled true
   action :create
