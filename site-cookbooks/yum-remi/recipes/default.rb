ab7aebf45a9bf420074ee82a66f8a0045a7b8a29
DQNEO <dqneoo@gmail.com>
2014-02-04 11:51:13 +0900

    add remi in --enablerpos. see http://blog.famillecollet.com/post/2014/01/11/PHP-5.4.24-and-5.5.8

--- site-cookbooks/yum-remi/recipes/default.rb
+++ site-cookbooks/yum-remi/recipes/default.rb
@@ -38,7 +38,7 @@ end
 
 %w{php php-pear php-mbstring php-xml php-devel php-mysql php-gd}.each do |pkg|
     package pkg do
-        options "--enablerepo=remi-php55"
+        options "--enablerepo=remi-php55,remi"
         action :install
     end
 end
