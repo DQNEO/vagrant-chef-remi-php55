8f2ed97ad583e9c32f8cfa3bfa7dbe8995acbd04
DQNEO <dqneoo@gmail.com>
2014-02-08 21:55:59 +0900

    this is not needed. add_recipe "yum" is enough.

--- site-cookbooks/yum-remi-php55/recipes/default.rb
+++ site-cookbooks/yum-remi-php55/recipes/default.rb
@@ -7,7 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "yum"
 include_recipe "yum-remi-php55::epel"
 include_recipe "yum-remi-php55::remi"
 include_recipe "yum-remi-php55::remi-php55"
